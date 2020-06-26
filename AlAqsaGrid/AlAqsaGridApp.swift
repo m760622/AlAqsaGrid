7224800a74e5de20e6697a53257f7e34b88f1f13
Mohammed Abunada <m7606225@gmail.com>
2020-06-26 02:42:55 +0200

    Initial Commit

---
+++ AlAqsaGrid/AlAqsaGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlAqsaGridApp.swift
+//  AlAqsaGrid
+//
+//  Created by Mohammed Abunada on 2020-06-26.
+//
+
+import SwiftUI
+
+@main
+struct AlAqsaGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
